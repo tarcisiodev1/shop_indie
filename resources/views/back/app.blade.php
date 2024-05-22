c619ddc62a16f6578c1346057e94f9077b6e2416
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2024-05-22 19:56:54 -0300

    navbar sidebar

--- resources/views/back/app.blade.php
+++ resources/views/back/app.blade.php
@@ -21,20 +21,26 @@
 <body class="sb-nav-fixed">
 
     <main class="py-4">
-        @yield('content')
+        @include('back.partials.navbar')
+        <div id="layoutSidenav">
+
+            @include('back.partials.sidebar')
+            @yield('content')
+        </div>
         <h1>main</h1>
     </main>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
-    <script src="js/scripts.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script> --}}
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     <script src="assets/demo/chart-area-demo.js"></script>
     <script src="assets/demo/chart-bar-demo.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
         crossorigin="anonymous"></script>
     <script src="js/datatables-simple-demo.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
