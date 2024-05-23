5b9034edd86a406d09f04ce4430b607ee8c80320
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2024-05-23 02:23:33 -0300

    layout

--- resources/views/back/main.blade.php
+++ resources/views/back/main.blade.php
@@ -11,28 +11,30 @@
     <title>@yield('title')</title>
 
     <!-- Fonts -->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/all.min.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
 
 <body class="sb-nav-fixed">
 
     <main class="py-4">
-        @include('back.partials.navbar')
+        @include('back.layout.navbar')
         <div id="layoutSidenav">
 
-            @include('back.partials.sidebar')
-            @yield('content')
+            @include('back.layout.sidebar')
+            <div id="layoutSidenav_content">
+                @yield('content')
+                @include('back.layout.footer')
+            </div>
         </div>
-        <h1>main</h1>
     </main>
     </div>
 
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
     <script src="assets/demo/chart-area-demo.js"></script>
