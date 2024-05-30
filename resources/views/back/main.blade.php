153278acb0ee1a8a0f6ed52f8a973408a67475c5
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2024-05-30 01:55:39 -0300

    view products

--- resources/views/back/main.blade.php
+++ resources/views/back/main.blade.php
@@ -13,7 +13,7 @@
     <!-- Fonts -->
     <!-- Incluindo a fonte Roboto do Google Fonts com peso 100 -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap" rel="stylesheet">
-    <link href="css/all.min.css" rel="stylesheet" />
+    <link href="{{ asset('css/all.min.css') }}" rel="stylesheet" />
     <link href="https://cdn.datatables.net/v/dt/jq-3.7.0/jszip-3.10.1/dt-2.0.7/b-3.0.2/b-html5-3.0.2/datatables.min.css"
         rel="stylesheet">
 
