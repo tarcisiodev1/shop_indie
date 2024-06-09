0afd3a1621d683f5b8820e8b6efb06d3a42ee684
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2024-06-09 15:33:04 -0300

    migrate products

--- database/migrations/2024_05_25_003427_create_products_table.php
+++ database/migrations/2024_05_25_003427_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->decimal('value', 8, 2);
+            $table->decimal('width', 8, 2);
+            $table->decimal('height', 8, 2);
+            $table->decimal('length', 8, 2);
+            $table->decimal('weight', 8, 2);
+            $table->string('image', 255);
             $table->timestamps();
         });
     }
