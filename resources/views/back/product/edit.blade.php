f9ae690de37f3b835d44daea415838b73b296b71
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2024-07-04 02:59:47 -0300

    change: view edit

--- resources/views/back/product/edit.blade.php
+++ resources/views/back/product/edit.blade.php
@@ -23,34 +23,41 @@
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label for="name" class="form-label">Nome do Produto</label>
-                                <input type="text" class="form-control" id="name" name="name">
+                                <input type="text" class="form-control" id="name" name="name"
+                                    value="{{ $productId->name }}">
                                 <div class="feedback" id="nameError"></div>
                             </div>
                             <div class="col-md-3 mb-3">
                                 <label for="value" class="form-label">Valor</label>
-                                <input type="number" class="form-control" id="value" name="value" step="0.01">
-                                <div class="feedback" id="valueError"></div>
+                                <input type="number" class="form-control" id="value" name="value" step="0.01"
+                                    value="{{ $productId->value }}">
+                                <div class="feedback" id="valueError">
+                                </div>
                             </div>
                             <div class="col-md-3 mb-3">
                                 <label for="weight" class="form-label">Peso</label>
-                                <input type="number" class="form-control" id="weight" name="weight" step="0.01">
+                                <input type="number" class="form-control" id="weight" name="weight"
+                                    value="{{ $productId->weight }}" step="0.01">
                                 <div class="feedback" id="weightError"></div>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-md-4 mb-3">
                                 <label for="width" class="form-label">Largura</label>
-                                <input type="number" class="form-control" id="width" name="width" step="0.01">
+                                <input type="number" class="form-control" id="width" name="width"
+                                    value="{{ $productId->width }}" step="0.01">
                                 <div class="feedback" id="widthError"></div>
                             </div>
                             <div class="col-md-4 mb-3">
                                 <label for="height" class="form-label">Altura</label>
-                                <input type="number" class="form-control" id="height" name="height" step="0.01">
+                                <input type="number" class="form-control" id="height" name="height"
+                                    value="{{ $productId->height }}" step="0.01">
                                 <div class="feedback" id="heightError"></div>
                             </div>
                             <div class="col-md-4 mb-3">
                                 <label for="length" class="form-label">Comprimento</label>
-                                <input type="number" class="form-control" id="length" name="length" step="0.01">
+                                <input type="number" class="form-control" id="length" name="length"
+                                    value="{{ $productId->length }}" step="0.01">
                                 <div class="feedback" id="lengthError"></div>
                             </div>
                         </div>
@@ -63,7 +70,7 @@
                             </div>
                         </div>
                         <div class="feedback" id="generalError"></div>
-                        <button type="submit" class="btn btn-primary">Cadastrar</button>
+                        <button type="submit" class="btn btn-primary">Editar</button>
                     </form>
 
 
